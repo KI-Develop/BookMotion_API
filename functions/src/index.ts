a990be8a1d7deac5f06ca9e7def2988ccf8bfeb3
peijun333 <unix.043@gmail.com>
2020-01-12 21:41:27 +0900

    make onRequest into onCall

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -97,42 +97,41 @@ export async function getGoogleBooks(keyword: string): Promise<any> {
   return res
 }
 
-export const googleBooksApi = functions.https.onRequest(
-  async (request, response) => {
-    const items: SearchData[] = []
+export const googleBooksApi = functions.https.onCall(async (data, context) => {
+  const keyword: string = data.keyword
+  // TODO: 認証してる人のみ返すようにする
+  // const uid = context.auth ? context.auth.uid : ""
+  const items: SearchData[] = []
 
-    if (request.query.keyword === undefined) {
-      response.status(400).send('keyword is undefined')
-    } else {
-      const keyword: string = request.query.keyword
+  if (keyword === undefined && !keyword) {
+    return { msg: 'keyword is undefined' }
+  } else {
+    await getGoogleBooks(keyword).then(res => {
+      if (res.data.items) {
+        for (const [index, item] of res.data.items.entries()) {
+          if (item.volumeInfo) {
+            items.push({
+              selfLink: item.selfLink,
+              title: item.volumeInfo.title || '',
+              authors: item.volumeInfo.authors || [],
+              description: item.volumeInfo.description || '',
+              publishedDate: item.volumeInfo.publishedDate || '',
+              publisher: item.volumeInfo.publisher || '',
+              totalPageCount: item.volumeInfo.pageCount || 0
+            })
 
-      await getGoogleBooks(keyword).then(res => {
-        if (res.data.items) {
-          for (const [index, item] of res.data.items.entries()) {
-            if (item.volumeInfo) {
-              items.push({
-                selfLink: item.selfLink,
-                title: item.volumeInfo.title || '',
-                authors: item.volumeInfo.authors || [],
-                description: item.volumeInfo.description || '',
-                publishedDate: item.volumeInfo.publishedDate || '',
-                publisher: item.volumeInfo.publisher || '',
-                totalPageCount: item.volumeInfo.pageCount || 0
-              })
-
-              if (item.volumeInfo.imageLinks) {
-                items[index].bookImage = item.volumeInfo.imageLinks.thumbnail
-              }
+            if (item.volumeInfo.imageLinks) {
+              items[index].bookImage = item.volumeInfo.imageLinks.thumbnail
             }
           }
         }
-      })
-      //TODO: firestoreからselfLinkが一致しているかを条件にして、一致していた場合は、重複していることがわかるようにパラメーターをつける.
-      if (items.length) {
-        response.status(200).send(items)
-      } else {
-        response.status(200).send('No items')
       }
+    })
+    //TODO: firestoreからselfLinkが一致しているかを条件にして、一致していた場合は、重複していることがわかるようにパラメーターをつける.
+    if (items.length) {
+      return { items }
+    } else {
+      return { msg: 'No items' }
     }
   }
-)
+})
